47f76f84655dd37f609584222dbc190762e2c691
liu.hongda <liu.hongda@trs.com.cn>
2019-04-24 15:41:35 +0800

    Initial commit

---
+++ src/linkedlist/LinkedList.java
@@ -0,0 +1,22 @@
+package linkedlist;
+
+import javax.xml.soap.Node;
+
+/**
+ * @author by liu.hongda
+ * @Classname linkedlist
+ * @Description TODO
+ * @Date 2019/4/24 14:13
+ */
+
+public class LinkedList {
+
+    public Object data;
+
+    public Node next;
+
+    public LinkedList(int data) {
+        this.data = data;
+    }
+
+}
---
+++ src/method/LinkList.java
@@ -0,0 +1,13 @@
+package method;
+
+/**
+ * @author by liu.hongda
+ * @Classname LinkList
+ * @Description TODO
+ * @Date 2019/4/24 15:20
+ */
+
+public class LinkList {
+
+
+}
